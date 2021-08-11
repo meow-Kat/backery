07c24ba32ae13d45d2624fffd381b9aec85f82e5
meow-Kat <76527080+meow-Kat@users.noreply.github.com>
2021-08-11 11:49:43 +0800

    update

---
+++ js/contain_us_button.js
@@ -0,0 +1,15 @@
+let form = document.querySelector('.form')
+let contain_us = document.querySelector('.contain-us-box')
+let join_family = document.querySelector('.join-family-box')
+let attachment = document.querySelector('.attachment')
+function active() {
+    let x = this.dataset.x
+    if (x == 1) {
+        attachment.style.display = 'none'
+    } else {
+        attachment.style.display = 'block'
+    }
+    form.classList.toggle('active')
+}
+contain_us.addEventListener('click', active)
+join_family.addEventListener('click', active)
