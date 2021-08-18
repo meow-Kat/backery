1b826abd520abf92ceba07144a8511aaa9e65eef
meow-Kat <76527080+meow-Kat@users.noreply.github.com>
2021-08-18 21:45:10 +0800

    update

--- js/menu.js
+++ js/menu.js
@@ -5,11 +5,10 @@ menu.onclick = function () {
     menuList.classList.toggle('open')
 }
 
-window.addEventListener('scroll',function () {
-    let firstSection = document.querySelector('.firstSection').offsetTop
-    if (window.scrollY > firstSection) {
-        menu.classList.add('show')
-    }else{
-        menu.classList.remove('show')
+let menuButton = document.querySelectorAll('.menuButton')
+menuButton.forEach(element => {
+    element.onclick = function () {
+        menu.classList.remove('active')
+        menuList.classList.remove('open')
     }
-})
+});
---
+++ js/scroll.js
@@ -0,0 +1,35 @@
+function scrollHander() {
+    let windowTop = window.scrollY
+    let windowBottom = windowTop + window.innerHeight
+
+    // menu
+    let firstSection = document.querySelector('.firstSection').offsetTop
+    if (window.scrollY > firstSection) {
+        menu.classList.add('show')
+    }else{
+        menu.classList.remove('show')
+    }
+
+    // 5th parallax
+    let parallax = document.querySelector('.parallax')
+    let fifthSectionTop = document.querySelector('.fifthSection').offsetTop
+    if ( fifthSectionTop + parallax.offsetTop + parallax.clientHeight / 2 < windowBottom) {
+        let calcNum =(fifthSectionTop - window.scrollY) / 100
+        parallax.style.top = calcNum*5  + '%'
+        if (parallax.style.top < '0') {
+            parallax.style.top = '0'
+        }
+    }
+
+    // 6th add animation
+    let sixthSectionTop = document.querySelector('.sixthSection').offsetTop
+    let containUs = document.querySelector('.containUs')
+    let containUsBtn = document.querySelector('.contain-us-box')
+    let joinFamilyBtn = document.querySelector('.join-family-box')
+    if (sixthSectionTop + containUs.clientHeight/2 < windowBottom) {
+        containUsBtn.classList.add('animation')
+        joinFamilyBtn.classList.add('animation')
+    }
+}
+
+window.addEventListener('scroll', scrollHander)
