144bcf78506384112ee3b5f1be1f90956a115dc0
meow-Kat <76527080+meow-Kat@users.noreply.github.com>
2021-08-23 20:34:39 +0800

    done

---
+++ js/modal.js
@@ -0,0 +1,18 @@
+let modal = document.querySelector('.modal')
+let newsBtn = document.querySelectorAll('.newsBtn')
+let close = document.querySelector('.close')
+newsBtn.forEach(element => {
+    element.onclick = function () {
+        modal.classList.add('show')
+    }
+});
+close.onclick = function () {
+    modal.classList.remove('show')
+}
+modal.addEventListener('click', function () {
+    let modalBord = document.querySelector('.modalBord')
+    modalBord.onclick = (event) => {
+        event.stopPropagation()
+    }
+    modal.classList.remove('show')
+})
